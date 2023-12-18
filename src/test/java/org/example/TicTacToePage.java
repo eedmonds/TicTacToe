b16f36c6cf78636d74c18bb1eeb34e054ee90749
Eric Edmonds <ericedmonds@icloud.com>
2023-12-18 18:17:24 -0500

    Commit clean up code

--- src/test/java/org/example/TicTacToePage.java
+++ src/test/java/org/example/TicTacToePage.java
@@ -3,9 +3,7 @@ package org.example;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.time.Duration;
 
 public class TicTacToePage {
     private final WebDriver driver;
